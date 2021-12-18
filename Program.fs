b3288c3b9cd7695a291ae457fcd7a2974b07ccf9
Jonas Thuresson <joth@netcompany.com>
2021-12-18 16:46:12 +0100

    Project setup

---
+++ Program.fs
@@ -0,0 +1,20 @@
+﻿open System.Xml
+open System.IO
+
+// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+// See the 'F# Tutorial' project for more help.
+
+// Define a function to construct a message to print
+let parseXml xmlPath =
+    let doc = new XmlDocument() in
+        File.ReadAllText(xmlPath)
+        |> doc.LoadXml
+    doc
+    
+
+[<EntryPoint>]
+let main argv =
+    let xml = parseXml "C:\\Users\\joth\\dev\\ssis-version-controller\\test\\FetchProduct.dtsx"
+    let xmlString = xml.ToString()
+    printfn "%s" xmlString
+    0 // return an integer exit code
